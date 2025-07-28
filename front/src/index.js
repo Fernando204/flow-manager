76b2b6641c351b07a365e4b726454a171fb27ff9
fernando <fernadotb2@gmail.com>
2025-07-28 14:52:20 -0300

    front initialized

---
+++ front/src/index.js
@@ -0,0 +1,11 @@
+const selectBtView = document.querySelector(".selectAreaBt");
+
+const gotoCaixa = ()=>{
+    selectBtView.style.marginLeft = "-200px";
+}
+const gotoClientRegister = ()=>{
+    selectBtView.style.marginLeft = "0px";
+}
+const gotoHistoric = ()=>{
+    selectBtView.style.marginLeft = "200px";
+}
