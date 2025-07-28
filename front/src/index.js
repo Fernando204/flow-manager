e0e655f3f7132c1a1dfd6054d138ab89404040c0
Fernando Amorim <fernadotb2@gmail.com>
2025-07-28 20:54:44 -0300

    area de vendas finalizada

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,21 @@
 const selectBtView = document.querySelector(".selectAreaBt");
+const vendaBox = document.querySelector(".vendaBox");
+const clientRegister = document.querySelector(".clientBox");
+
+let openBox = vendaBox;
 
 const gotoCaixa = ()=>{
     selectBtView.style.marginLeft = "-200px";
+    openBox.style.opacity = "0";
+    vendaBox.style.opacity = "1";
+    openBox = vendaBox;
 }
 const gotoClientRegister = ()=>{
     selectBtView.style.marginLeft = "0px";
+    openBox.style.opacity = "0";
+    clientRegister.style.opacity = "1";
+    openBox = clientRegister;
+
 }
 const gotoHistoric = ()=>{
     selectBtView.style.marginLeft = "200px";
